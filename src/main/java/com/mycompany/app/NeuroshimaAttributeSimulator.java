a711f7033379de7a2a30050f29e1ce47cdf08152
Mateusz <mateuszglass@gmail.com>
2018-05-30 16:02:06 +0200

    Exercise done after 5 minutes. thx for yesterday advice

--- src/main/java/com/mycompany/app/NeuroshimaAttributeSimulator.java
+++ src/main/java/com/mycompany/app/NeuroshimaAttributeSimulator.java
@@ -3,33 +3,27 @@ package com.mycompany.app;
 import java.util.*;
 
 public class NeuroshimaAttributeSimulator {
-	
+
 	public NeuroshimaAttributeSimulator() {
 	}
-	
+
 	public List<Integer> roll() {
 		Random random = new Random();
-		List<Integer> sixRollResult = new ArrayList<>();
-		int begin = 1;
+		List<Integer> highest5RollResults = new ArrayList<>();
 		for (int startPoint = 1; startPoint <= 6; startPoint++) {
-			while (begin <= 3) {
-				List<Integer> partialyResults = new LinkedList<>();
-				int rollResult = random.nextInt(20) + 1;
-				partialyResults.add(rollResult);
-				if (begin == 3) {
-					int averageRollResult = (partialyResults[0] + partialyResults[1] + partialyResults[2])/ 3;
-					return sixRollResult.add(averageRollResult);
-				} else {
-					begin++;
-					return begin;
-				}
-			}
-//			sixRollResult.add(singleRollResult);
+			int firstRollResult = random.nextInt(20) + 1;
+			int secondRollResult = random.nextInt(20) + 1;
+			int thirdRollResult = random.nextInt(20) + 1;
+			int averageRollResult = (firstRollResult + secondRollResult + thirdRollResult) / 3;
+			Math.ceil(averageRollResult);
+			highest5RollResults.add(averageRollResult);
 		}
-		return sixRollResult;
+		Collections.sort(highest5RollResults);
+		highest5RollResults.remove(0);
+		return highest5RollResults;
 	}
-	
-	public static void main (String[] args) {
+
+	public static void main(String[] args) {
 		List<Integer> fiveAttributes = new NeuroshimaAttributeSimulator().roll();
 		System.out.println("This is your 5 highest roll for attributes" + fiveAttributes);
 	}
