14125c287f5788232cc50fa249cb19eb0d5d6903
Mateusz <mateuszglass@gmail.com>
2018-06-01 17:44:39 +0200

    Bug has been removed.

--- src/main/java/com/mycompany/app/NeuroshimaAttributeSimulator.java
+++ src/main/java/com/mycompany/app/NeuroshimaAttributeSimulator.java
@@ -18,6 +18,28 @@ public class NeuroshimaAttributeSimulator {
 		}
 		Collections.sort(highest5RollResults);
 		highest5RollResults.remove(0);
+		for (int index = 0; index <= 4; index++) {
+			if (highest5RollResults.get(index) < 6) {
+				highest5RollResults.remove(index);
+				highest5RollResults.add(6);
+			}
+		}
+		/*if (highest5RollResults.get(0) < 6) {
+			//highest5RollResults.replaceAll(operator);
+		} else if (highest5RollResults.get(1) < 6) {
+			highest5RollResults.remove(1);
+			highest5RollResults.add(6);
+		} else if (highest5RollResults.get(2) < 6) {
+			highest5RollResults.remove(2);
+			highest5RollResults.add(6);
+		} else if (highest5RollResults.get(3) < 6) {
+			highest5RollResults.remove(3);
+			highest5RollResults.add(6);
+		}  else (highest5RollResults.get(4) < 6) {
+			highest5RollResults.remove(4);
+			highest5RollResults.add(6);
+		}*/
+		Collections.sort(highest5RollResults);
 		return highest5RollResults;
 	}
 
