37867dce3df87ebf1e693695035affe704021a03
Mateusz <mateuszglass@gmail.com>
2018-06-19 18:14:00 +0200

    this is commit to show you how I care with this bug. Next commit will get only what is need

--- src/main/java/com/mycompany/app/NeuroshimaAttributeSimulator.java
+++ src/main/java/com/mycompany/app/NeuroshimaAttributeSimulator.java
@@ -11,7 +11,10 @@ public class NeuroshimaAttributeSimulator {
 			int firstRollResult = random.nextInt(20) + 1;
 			int secondRollResult = random.nextInt(20) + 1;
 			int thirdRollResult = random.nextInt(20) + 1;
-			double averageRollResult = (firstRollResult + secondRollResult + thirdRollResult) / 3;
+			double averageRollResult1 = (firstRollResult + secondRollResult + thirdRollResult) / 3;
+			System.out.println(averageRollResult1);			
+			double averageRollResult = ((double)firstRollResult + (double)secondRollResult + (double)thirdRollResult) / 3;
+			System.out.println(averageRollResult);
 			double roundedAvg = Math.ceil(averageRollResult);
 			int oneResult = (int) roundedAvg;
 			highest5RollResults.add(oneResult);
